65d4cf21d23683dcb83ff035f5d560f82cdf8e76
吕军 <877896985@qq.com>
2020-04-06 21:33:15 +0800

    回滚提交

--- demo/demo.py
+++ demo/demo.py
@@ -3,10 +3,7 @@ class A(object)：
 	age = 30
 
 	def get_name(self)：
-		print("haha")
-		pass
 		return self.name
-
 class A(object)：
 	pass
 
@@ -20,8 +17,7 @@ class NiBi(object)：
 class NB(object)：
 	name = "NB"
 	age = "1000000"
-	xixi = "xixi"
 
 	def get_age(self)：
 		return self.age
-
+		
