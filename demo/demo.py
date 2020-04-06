1bb37db1cf8dd5457e7e2fdc8755a70d8a3a7232
吕军 <877896985@qq.com>
2020-04-06 20:20:19 +0800

    no message

--- demo/demo.py
+++ demo/demo.py
@@ -4,3 +4,13 @@ class A(object)：
 
 	def get_name(self)：
 		return self.name
+class A(object)：
+	pass
+
+class NiBi(object)：
+	name = "NIBI"
+	age = "10000000000000000000000"
+
+	def get_age(self)：
+		return self.age
+		
